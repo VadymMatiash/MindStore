006220dc3d46f6eaf08ffac6a5548280b7bdda64
VadymMatiash <vadym.matiash@gmail.com>
2018-12-16 22:58:11 +0200

    Created registration route with password hashing

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
 
 const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const db = require('./config/keys').mongoURI;
 
+//DB connection
 mongoose
     .connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
